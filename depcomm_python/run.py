ada1caf1bd8dbfa32e54d9b5b454873c4c2fc040
ieeesp2021sub <75189135+ieeesp2021sub@users.noreply.github.com>
2020-12-17 10:01:49 +0800

    Update run.py

--- depcomm_python/run.py
+++ depcomm_python/run.py
@@ -1,6 +1,6 @@
 import os
 
-inputlog = 'leak_data.txt' #log
+inputlog = 'leak_data.txt' #log file name
 POI = '10.10.103.10:38772\-\>159.226.251.11:25' #POI
 
 os.system('python2 core/Start.py '+inputlog+' '+POI)
